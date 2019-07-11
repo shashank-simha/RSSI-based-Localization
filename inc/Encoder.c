5c0890d5548f61c55f7b4dbc65364b61d65ab5f9
shashank-simha <shashanksimha183@gmail.com>
2019-07-11 20:54:51 +0530

    Encoder Lib

---
+++ inc/Encoder.c
@@ -0,0 +1,110 @@
+/*
+ * Encoder.c
+ *
+ *  Created on: 09-Jul-2019
+ *      Author: simha
+ */
+
+ /*
+  *
+  * Pololu #3542 Romi Encoder connected to Pololu #3543 Motor Driver and Power Distribution Board
+  * This connects motor, power, encoders, and grounds.  Kit includes this hardware.  See images.
+  * Sever VPU = VREG jumper on Motor Driver and Power Distribution Board and connect VPU to 3.3V.
+  * This is necessary because MSP432 inputs are not 5V tolerant.
+  *
+  * Left Encoder A connected to P6.4 (J1)                     .
+  * Left Encoder B connected to P6.5 (J1)
+  * Right Encoder A connected to P5.4 (J3)
+  * Right Encoder B connected to P5.5 (J3)
+  *
+  * Pololu encoder has 12 counts per revolution (counting all 4 edges).
+  * The motor has a gearbox with a 120:1 ratio.
+  * This gives 12*120 = 1440 counts per revolution of the wheel.
+  * Since we are only counting 1 edge of the encoder we need to divide by 4 for a total of 360 counts per revolution.
+ */
+
+
+
+#include <stdint.h>
+#include "msp.h"
+#include <ti/devices/msp432p4xx/driverlib/driverlib.h>
+
+// Initialize Encoder inputs
+
+static int left_motor_count = 0;
+static int right_motor_count = 0;
+
+void Encoder_Init(void){
+    /* Configuring P6.4 as an input and enabling interrupts */
+    MAP_GPIO_setAsInputPin(GPIO_PORT_P6, GPIO_PIN4);
+    MAP_GPIO_interruptEdgeSelect(GPIO_PORT_P6, GPIO_PIN4, GPIO_LOW_TO_HIGH_TRANSITION);
+    MAP_GPIO_clearInterruptFlag(GPIO_PORT_P6, GPIO_PIN4);
+    MAP_GPIO_enableInterrupt(GPIO_PORT_P6, GPIO_PIN4);
+    MAP_Interrupt_enableInterrupt(INT_PORT6);
+
+    MAP_GPIO_setAsInputPin(GPIO_PORT_P6, GPIO_PIN5);
+
+
+    /* Configuring P5.4 as an input and enabling interrupts */
+    MAP_GPIO_setAsInputPin(GPIO_PORT_P5, GPIO_PIN4);
+    MAP_GPIO_interruptEdgeSelect(GPIO_PORT_P5, GPIO_PIN4, GPIO_LOW_TO_HIGH_TRANSITION);
+    MAP_GPIO_clearInterruptFlag(GPIO_PORT_P5, GPIO_PIN4);
+    MAP_GPIO_enableInterrupt(GPIO_PORT_P5, GPIO_PIN4);
+    MAP_Interrupt_enableInterrupt(INT_PORT5);
+
+    MAP_GPIO_setAsInputPin(GPIO_PORT_P5, GPIO_PIN5);
+
+
+    /* Enable Master Interrupt */
+    MAP_Interrupt_enableMaster();
+}
+
+int Get_Left_Motor_Count() {
+    return left_motor_count;
+}
+
+int Get_Right_Motor_Count() {
+    return right_motor_count;
+}
+
+/* GPIO ISR */
+void PORT6_IRQHandler(void)
+{
+    uint32_t status;
+
+    status = MAP_GPIO_getEnabledInterruptStatus(GPIO_PORT_P6);
+    MAP_GPIO_clearInterruptFlag(GPIO_PORT_P6, status);
+
+    /* Toggling the output on the LED */
+    if(status & GPIO_PIN4)
+    {
+        if (MAP_GPIO_getInputPinValue(GPIO_PORT_P6, GPIO_PIN5))
+            left_motor_count++;
+        else
+            left_motor_count--;
+
+        MAP_GPIO_toggleOutputOnPin(GPIO_PORT_P2, GPIO_PIN2);
+    }
+
+}
+
+/* GPIO ISR */
+void PORT5_IRQHandler(void)
+{
+    uint32_t status;
+
+    status = MAP_GPIO_getEnabledInterruptStatus(GPIO_PORT_P5);
+    MAP_GPIO_clearInterruptFlag(GPIO_PORT_P5, status);
+
+    /* Toggling the output on the LED */
+    if(status & GPIO_PIN4)
+    {
+        if (MAP_GPIO_getInputPinValue(GPIO_PORT_P5, GPIO_PIN5))
+            right_motor_count++;
+        else
+            right_motor_count--;
+        MAP_GPIO_toggleOutputOnPin(GPIO_PORT_P2, GPIO_PIN1);
+    }
+
+}
+
---
+++ inc/Encoder.h
@@ -0,0 +1,17 @@
+/*
+ * Encoder.h
+ *
+ *  Created on: 09-Jul-2019
+ *      Author: simha
+ */
+
+#ifndef ENCODER_H_
+#define ENCODER_H_
+
+
+void Encoder_Init(void);
+int Get_Left_Motor_Count();
+int Get_Right_Motor_Count();
+
+
+#endif /* ENCODER_H_ */
